5dc9a3b2e8f06f002bb0a77bf8095e7f9f67db7e
Bueno-Italo <italo-bueno@hotmail.com>
2025-01-12 17:38:18 -0300

    Adiconando arquivo de fundamentos

---
+++ teoria/fundamentos.js
@@ -0,0 +1,15 @@
+// Exemplos de declaração de variáveis
+
+// var nome = "valor"
+var minhaVariavel = "Olá, mundo!"
+
+console.log(minhaVariavel)
+
+const nome = "Italo";
+let idade = 31;
+var ativo = true;
+
+// Tipos de dados
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof ativo);
