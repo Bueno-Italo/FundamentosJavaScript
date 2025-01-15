b2ffd80da4d93d3473ca7384b5183c0aaf38ffa8
Bueno-Italo <italo-bueno@hotmail.com>
2025-01-14 21:12:17 -0300

    Praticando Switch

--- teoria/fundamentos.js
+++ teoria/fundamentos.js
@@ -99,7 +99,34 @@ switch(fruta) {
         break;
 };
 
+const diaDaSemana = 3;
 
+switch (diaDaSemana) {
+    case 1:
+        console.log("Hoje é Domingo!");
+        break; // O `break` evita que os outros casos sejam executados.
+    case 2:
+        console.log("Hoje é Segunda-feira!");
+        break;
+    case 3:
+        console.log("Hoje é Terça-feira!");
+        break;
+    case 4:
+        console.log("Hoje é Quarta-feira!");
+        break;
+    case 5:
+        console.log("Hoje é Quinta-feira!");
+        break;
+    case 6:
+        console.log("Hoje é Sexta-feira!");
+        break;
+    case 7:
+        console.log("Hoje é Sábado!");
+        break;
+    default:
+        // O `default` é executado se nenhum caso corresponder.
+        console.log("Valor inválido para dia da semana.");
+};
 
 
 
