67b663c8963caecb169b93a0aef001759a083dc1
Bueno-Italo <italo-bueno@hotmail.com>
2025-01-25 19:02:17 -0300

    Exercicio de função

--- teoria/fundamentos_exercicios.js
+++ teoria/fundamentos_exercicios.js
@@ -30,3 +30,28 @@ for(let i = 0; i < meuArray.length; i++) {
 
     console.log(meuArray[i]);
 }
+
+// === Exercicio 5: ===
+// Crie uma função que aceite uma string representando um formato de 24 horas (por exemplo, "14:30"). A função deve retornar uma string que converta o horario para o formato 12 horas (por exemplo, 2:30 PM). Use o objeto Math para auxiliar nessa conversão. Certifique-se de que sua função lida corretamente com o horario da meia-noite e meio-dia;
+const agora = new Date();
+const horas = agora.getHours();
+const minutos = agora.getMinutes();
+
+function mostrarHoraAtual() {
+
+    const hora12 = horas % 12 || 12;
+    const periodo = horas >= 12 ? "PM" : "AM";
+    
+    if(horas < 12){
+        console.log("bom dia!");
+    } else if (horas < 18) {
+        console.log("boa tarde!");
+    } else {
+        console.log("Boa noite!");
+    }
+
+    //console.log(`A hora exata é ${horas}:${minutos}`)
+    console.log(`A hora exata é ${hora12}:${minutos.toString().padStart(2, '0')} ${periodo}`);
+}
+
+mostrarHoraAtual();
