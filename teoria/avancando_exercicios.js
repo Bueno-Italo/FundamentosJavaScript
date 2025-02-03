f187658e2433cf99fb9ef46a11ecc00714f147d4
Bueno-Italo <italo-bueno@hotmail.com>
2025-02-03 19:54:18 -0300

    Exercicios de manipulação de arrays

--- teoria/avancando_exercicios.js
+++ teoria/avancando_exercicios.js
@@ -16,7 +16,7 @@ listaCompras.shift();
 
 console.log(listaCompras)
 
-// === Exercicio de Manipulação de Array - find()
+// === Exercicio de Manipulação de Array - find() ===
 // Você tem um array de nuúmeros: [3, 7, 14, 21, 29, 36, 42]
 // Use a função find() para encontrar o primeiro numero que é divisivel por 7 e maior que 10.
 
@@ -29,3 +29,26 @@ console.log(numerosArray);
 const numeroEncontrado = numerosArray.find((num) => num > 10 && num % 7 === 0);
 
 console.log(numeroEncontrado)
+
+
+// === Exercicio de Manipulação de Array - find() ===
+
+// Dado um array de números: [5, 10, 15, 20, 25, 30, 35]
+// Use a função filter() para criar um novo array que contenha apenas os numeros maiores que 20.
+const arrayNum = [5, 10, 15, 20, 25, 30, 35, 40];
+
+console.log(arrayNum);
+
+const numMaior = arrayNum.filter((arrayNum) => arrayNum > 20);
+
+console.log(numMaior);
+
+// === Exercicio de Manipulação de Array - split(), trim() ===
+// Dada a string: " Bom dia, mundo! "
+// Remova os espaços em branco no início e no final da string em seguida, divida a string em palavras.
+
+const frase = " Bom dia, mundo! ";
+
+const palavras = frase.trim().split(" ");
+
+console.log(palavras);
