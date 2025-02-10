ab75971b25ccb44e7cc528a551026fb10065166c
Bueno-Italo <italo-bueno@hotmail.com>
2025-02-10 18:50:04 -0300

    Manipulando atributos com DOM

--- teoria/dom.js
+++ teoria/dom.js
@@ -4,3 +4,28 @@
 const elementoPorId = document.getElementById("meuId");
 
 console.log(elementoPorId);
+
+const elementoPorId2 = document.querySelector("#meuId");
+
+console.log(elementoPorId2);
+
+// Manipular conteudo de texto
+
+const element = document.querySelector("#meuId");
+
+console.log(element.textContent);
+
+setTimeout(() => {
+    element.textContent = "Mudei o texto teste";
+}, 1000);
+
+
+// Trabalhando com atributos
+const link = document.querySelector("a");
+
+console.log(link);
+
+// Trocando o atributo pelo DOM
+link.setAttribute("href", "https://horadecodar.com.br/");
+
+console.log(link.getAnimations("href"));
