df3512a90747a34775b0c070293396aacc4e8a27
Bueno-Italo <italo-bueno@hotmail.com>
2025-04-21 20:09:37 -0300

    Adicinando logica JS na aplicação

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,1 +1,30 @@
-console.log("teste")
+// Seleção Elementos
+const display = document.querySelector("#displayInput");
+const botaoigual = document.querySelector(".igual");
+const botaoPonto = document.querySelector(".ponto");
+const botoesNumeros = document.querySelectorAll(".num");
+const botoesOperadores = document.querySelector(".operador");
+
+// Variaveis globais
+let operacaoAtual = ""; 
+let operador = null; 
+let valorAnterior = ""; 
+let calculando = false;
+
+function atualizaDisplay() {
+    display.value = operacaoAtual;
+}
+
+function insereNumero(evento) {
+    if(calculando) {
+        operacaoAtual = evento.target.textContent;
+        calculando = false;
+    } else {
+        operacaoAtual += evento.target.textContent;
+    }
+
+    atualizaDisplay();
+}
+
+// Eventos
+botoesNumeros.forEach((botao) => botao.addEventListener("click", insereNumero));
