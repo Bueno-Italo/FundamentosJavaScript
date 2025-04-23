41b2c46e4b10eba28f0b97ce954675f3323ba48d
Bueno-Italo <italo-bueno@hotmail.com>
2025-04-22 23:39:28 -0300

    Trabalhando com funções

--- calculadora/script.js
+++ calculadora/script.js
@@ -3,7 +3,7 @@ const display = document.querySelector("#displayInput");
 const botaoigual = document.querySelector(".igual");
 const botaoPonto = document.querySelector(".ponto");
 const botoesNumeros = document.querySelectorAll(".num");
-const botoesOperadores = document.querySelector(".operador");
+const botoesOperadores = document.querySelectorAll(".operador");
 
 // Variaveis globais
 let operacaoAtual = ""; 
@@ -26,5 +26,54 @@ function insereNumero(evento) {
     atualizaDisplay();
 }
 
+function inserePonto() {
+    if(operacaoAtual.indexOf(".") === -1) {
+        operacaoAtual += ".";
+        atualizaDisplay();
+    }
+}
+
+function insereOperador(evento) {
+    if(operacaoAtual !== "") {
+        if(!calculando) {
+            if(operador !== null) {
+                calcula();
+            }
+            valorAnterior = operacaoAtual;
+            operacaoAtual = "";
+        }
+        operador = evento.target.textContent;
+    }
+}
+
+function calcula() {
+    let resultado = null;
+    const operandoAnterior = parseFloat(valorAnterior);
+    const operandoAtual = parseFloat(operacaoAtual);
+
+    switch(operador) {
+        case "+":
+            resultado = operandoAnterior + operandoAtual;
+            break;
+        case "-":
+            resultado = operandoAnterior - operandoAtual;
+            break;
+        case "*":
+            resultado = operandoAnterior * operandoAtual;
+            break;  
+        case "/":
+            resultado = operandoAnterior / operandoAtual;
+            break;  
+    }
+
+    operacaoAtual = String(resultado);
+    valorAnterior = operacaoAtual;
+    calculando = true;
+    atualizaDisplay();
+}
+
 // Eventos
+botaoPonto.addEventListener("click", inserePonto);
 botoesNumeros.forEach((botao) => botao.addEventListener("click", insereNumero));
+botoesOperadores.forEach((botao) => botao.addEventListener("click", insereOperador));
+botaoigual.addEventListener("click", calcula);
